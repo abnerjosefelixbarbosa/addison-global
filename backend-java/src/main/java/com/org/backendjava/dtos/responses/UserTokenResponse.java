0568ee65f2f0261d15737313f587722b08a155fd
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2024-04-10 17:43:08 -0300

    commited by Abner

--- backend-java/src/main/java/com/org/backendjava/dtos/responses/UserTokenResponse.java
+++ backend-java/src/main/java/com/org/backendjava/dtos/responses/UserTokenResponse.java
@@ -1,9 +1,6 @@
 package com.org.backendjava.dtos.responses;
 
-import java.time.Instant;
 import java.util.Date;
-
-import org.hibernate.validator.internal.constraintvalidators.bv.time.futureorpresent.FutureOrPresentValidatorForInstant;
 
 import com.org.backendjava.entities.User;
 
@@ -23,7 +20,6 @@ public class UserTokenResponse {
 	
 	public void setUserTokenResponseWithDateTime(User user) {
 		Date date = new Date();
-		Instant instant = Instant.now();
 		this.token = user.getName() + "_" + date.toString();
 	}
 }
